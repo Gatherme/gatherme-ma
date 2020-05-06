9134ede2da617cbaf35acf2445b370131d64bcbc
Edder <edhernandezfo@unal.edu.co>
2020-05-05 21:17:31 -0500

    overlay

---
+++ gatherme/app/src/main/java/com/example/gatherme/UserFeed/ViewModel/ProfileViewModel.java
@@ -0,0 +1,7 @@
+package com.example.gatherme.UserFeed.ViewModel;
+
+import androidx.lifecycle.ViewModel;
+
+public class ProfileViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
